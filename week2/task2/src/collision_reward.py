6e91e0dc3362a3191b439cff75eef45622e35d3e
pramodwakshe <pramodwakshe17@gmail.com>
2026-09-23 13:16:32 +0530

    Implement drone collision and boundary penalty

--- week2/task2/src/collision_reward.py
+++ week2/task2/src/collision_reward.py
@@ -20,3 +20,41 @@ def calculate_distance(position_a, position_b):
     dz = position_a[2] - position_b[2]
 
     return sqrt(dx**2 + dy**2 + dz**2)
+
+def is_outside_boundary(position):
+    """Check whether a drone position is outside the 3D environment."""
+
+    x, y, z = position
+
+    return not (
+        SPACE_BOUNDS["x"][0] <= x <= SPACE_BOUNDS["x"][1]
+        and SPACE_BOUNDS["y"][0] <= y <= SPACE_BOUNDS["y"][1]
+        and SPACE_BOUNDS["z"][0] <= z <= SPACE_BOUNDS["z"][1]
+    )
+
+
+def has_drone_collision(position, other_positions):
+    """Check whether a drone collides with another drone."""
+
+    for other_position in other_positions:
+        distance = calculate_distance(position, other_position)
+
+        if distance <= COLLISION_DISTANCE:
+            return True
+
+    return False
+
+
+def calculate_collision_penalty(position, other_positions):
+    """
+    Return -100 when a drone hits another drone
+    or goes outside the environment boundary.
+    """
+
+    if is_outside_boundary(position):
+        return COLLISION_PENALTY
+
+    if has_drone_collision(position, other_positions):
+        return COLLISION_PENALTY
+
+    return 0
